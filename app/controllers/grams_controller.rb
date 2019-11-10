6fd703cd26ad4e620882e007b3d587e83423f82d
yehoom <zjzzhou@ucdavis.edu>
2019-11-10 09:57:49 +0000

    123

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,7 @@
 class GramsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
     
+    
     def new
         @gram = Gram.new
     end
@@ -9,13 +10,36 @@ class GramsController < ApplicationController
     def index
     end
     
+     def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank? 
+    end
+    
     def create
         @gram = current_user.grams.create(gram_params)
-        @gram = Gram.create(gram_params)
+       # @gram = Gram.create(gram_params)
             if @gram.valid?
         redirect_to root_path
       else
         render :new, status: :unprocessable_entity
+      end
+    end
+    
+    
+    def edit
+        @gram = Gram.find_by_id(params[:id])
+         return render_not_found if @gram.blank? 
+    end
+    
+    def update
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+
+        @gram.update_attributes(gram_params)
+        if @gram.valid?
+           redirect_to root_path
+        else
+        return render :edit, status: :unprocessable_entity
       end
     end
     
@@ -24,5 +48,8 @@ class GramsController < ApplicationController
   def gram_params
     params.require(:gram).permit(:message)
   end
-
+  
+  def render_not_found 
+    render plain: 'Not Found :(', status: :not_found
+  end
 end
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+
+    describe "grams#update action" do
+        it "should allow users to successfully update grams" do
+            gram = FactoryBot.create(:gram, message: "Initial Value")
+            patch :update, params: { id: gram.id, gram: { message: 'Changed'}}
+            expect(response).to redirect_to root_path
+            gram.reload
+            expect(gram.message).to eq "Changed"
+        end
+    
+        it "should have http 404 error if the gram cannot be found" do
+            patch :update, params: { id: "YOLOSWAG", gram: { message: 'Changed' } }
+            expect(response).to have_http_status(:not_found)
+        end
+    
+        it "should render the edit form with an http status of unprocessable_entity" do
+         gram = FactoryBot.create(:gram, message: "Initial Value")
+         patch :update, params: { id: gram.id, gram: { message: '' } }
+         expect(response).to have_http_status(:unprocessable_entity)
+         gram.reload
+         expect(gram.message).to eq "Initial Value"
+        end
+      end
+
+    
+    
+    describe "grams#edit action" do
+    it "should successfully show the edit form if the gram is found" do
+        gram = FactoryBot.create(:gram)
+        get :edit, params: {id: gram.id}
+        expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error message if the gram is not found" do
+        get :edit, params: {id: 'SWAG'}
+        expect(response).to have_http_status(:not_found)
+    end
+  end
+    
+    
+ 
+    
+    describe "grams#show action" do
+    it "should successfully show the page if the gram is found" do
+        gram = FactoryBot.create(:gram)
+        get :show, params: { id: gram.id }
+        expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do
+        get :show, params: { id: 'TACOCAT' }
+        expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
@@ -10,11 +65,10 @@ RSpec.describe GramsController, type: :controller do
 
 
   describe "grams#new action" do
-  
-  it "should require users to be logged in" do
-    get :new
-    expect(response).to redirect_to new_user_session_path
-  end
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
 
     it "should successfully show the new form" do
       user = FactoryBot.create(:user)
@@ -26,13 +80,13 @@ RSpec.describe GramsController, type: :controller do
   end
 
 
-describe "grams#create action" do
-  
-  it "should require users to be logged in" do
-    post :create, params: { gram: { message: "Hello" } }
-    expect(response).to redirect_to new_user_session_path
-  end
-  
+  describe "grams#create action" do
+
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "should successfully create a new gram in our database" do
       user = FactoryBot.create(:user)
       sign_in user
